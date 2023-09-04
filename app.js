9c60beef14d1ff82f3bb761da6e8247d71620533
pbokolo <dev.pbokolo@gmail.com>
2023-09-04 19:19:41 +0200

    Setting up the app.js file

---
+++ app.js
@@ -0,0 +1,5 @@
+"use script";
+
+const upload = document.querySelector(".upload");
+const fileInput = document.querySelector("#fileInput");
+const imgPreview = document.querySelector("#imgPreview");
